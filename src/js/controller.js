081d761eb8319c48f65988e8be2e3b59e4430a2a
Mikiyas6 <Mikitedi9@gmail.com>
2024-09-01 01:29:28 +0300

    I added helpers and configuration files

--- src/js/controller.js
+++ src/js/controller.js
@@ -4,21 +4,12 @@ import recipeView from './views/recipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
 
 const controlRecipes = async function (event) {
   try {
-    console.log(event);
     // const id = event.newURL.split('#')[1];
     const id = window.location.hash.slice(1);
     if (!id) return;
